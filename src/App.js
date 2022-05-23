36c973663c19444f6ff8d6d1fccab068f59fb35d
Dasia <dasiasmith023@gmail.com>
2022-05-22 23:14:18 -0500

    Added data constants and rendered error messages

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
     });
   }, []);
 
-  const setData = ({ url, name, public_repos, public_gists, followers, following, created_at }) => {
-    setUrl(url);
+  const setData = ({ html_url, name, public_repos, public_gists, followers, following, created_at }) => {
+    setUrl(html_url);
     setName(name);
     setRepos(public_repos);
     setGists(public_gists);
@@ -33,12 +33,43 @@ function App() {
 
   }
 
+  const handleSearch = (e) => {
+    setUserInput(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.message) {
+        setError(data.message)
+      }
+      else {
+        setData(data);
+        setError(null);
+      }
+    })
+  }
+
   return (
     <div>
      <div className='navbar'>GitHub Search</div>
      <div className="App">
-      <SearchBar placeholder={"Type username"}/>
-    </div>
+     <div className="searchInputs">
+        <input type={"text"} placeholder={"Type username"} onChange={handleSearch} onSubmit={handleSubmit}/>
+        <div className="seachIcon"></div>
+      </div>
+      { error ? (<h1>{error}</h1>) : (      
+      <div>
+        <h1>Link: {url}</h1>
+        <h1>Name: {name}</h1>
+        <h1>Repos: {repos}</h1>
+        <h1>Gists: {gists}</h1>
+        <h1>Followers: {followers}</h1>
+        <h1>Following: {following}</h1>
+        <h1>Date Created: {created}</h1>
+      </div>)}
+    </div> 
     </div>
   );
 }
